fb9366aa2074fbbec375cf59cba4ac263bd96820
Kumaramanivel <rkumaramanivel@gmail.com>
2018-06-10 10:51:04 +0530

    Initial commit

---
+++ Assets/Scripts/StartParticleStream.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartParticleStream : MonoBehaviour {
+
+	public GameObject ps;
+
+	void Start(){
+		ps.SetActive (false);
+	}
+
+	public void StartPS () {
+		ps.SetActive (true);
+		Debug.Log("started");
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+Unity 5.5.1p3
+Tested on Nougat.
